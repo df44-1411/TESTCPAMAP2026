0c0ccecb1b7173b5d7ff42abdbf6ddc3cda1d38a
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-06-14 00:47:28 +0100

    outside

--- map.js
+++ map.js
@@ -884,6 +884,7 @@ Highcharts.mapChart('container', {
     // --- TOOLTIP INTELIGENTE (LÊ AS CORES AUTOMATICAMENTE) ---
     tooltip: {
         useHTML: true,
+        outside: true,
         backgroundColor: 'transparent',
         borderWidth: 0,
         shadow: false,
