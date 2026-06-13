18fbe25e466846d34b666b50910f32a19337b1b4
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-06-14 00:42:00 +0100

    testing the fix to the tooltips

--- map.js
+++ map.js
@@ -884,13 +884,12 @@ Highcharts.mapChart('container', {
     // --- TOOLTIP INTELIGENTE (LÊ AS CORES AUTOMATICAMENTE) ---
     tooltip: {
         useHTML: true,
-        outside: true,
         backgroundColor: 'transparent',
         borderWidth: 0,
         shadow: false,
         padding: 0,
         followPointer: true,
-        headerFormat: '', 
+        headerFormat: '',
         formatter: function() {
             const point = this.point;
             
